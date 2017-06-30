3210fc9e53e7bb2dfd0bd62d11bce7ac069b35d5
Eva <marinievaluna@gmail.com>
2017-06-30 11:43:19 -0700

    added react charts

--- components/Chart.jsx
+++ components/Chart.jsx
@@ -1,15 +1,31 @@
 import React from 'react'
-import Bar from './Bar.jsx'
+import {Bar} from 'react-chartjs'
+
+const getChartData = (d1, d2) => {
+    return {
+        labels: ["HTTP/1.1", "HTTP/2"],
+        datasets: [{
+            label: 'Speed in ms',
+            data: [d1, d2],
+            fillColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)'
+            ],
+            strokeColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)'
+            ],
+            borderWidth: 1
+        }]
+    }
+}
 
 const Chart = (props) => {
-    console.log("chart props :", props)
+    let charData = getChartData(props.data.http1, props.data.http2)
+
     return (
         <div> 
-            <h1>chart </h1>
-            <svg width={300} height={200}> 
-                <Bar data={props.data.http1}/>
-                <Bar data={props.data.http2}/>
-            </svg>
+            <Bar data={charData} width={600} height={400}/>
         </div>
     )
 }
