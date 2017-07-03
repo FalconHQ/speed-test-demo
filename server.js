554dc1b3e9c20e1cbe14205622c22086ad76df1b
Poyu Chen <p820316@gmail.com>
2017-07-03 14:44:29 -0700

    uses https

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const https = require('https');
 const fs = require('fs')
 const path = require('path')
 const debug = require('debug') ('demo_server')
@@ -8,7 +9,11 @@ const PORT = 3000;
 
 app.use(express.static("."))
 
-app.listen(PORT, (err) => {   
+https.createServer({
+    key: fs.readFileSync('./certs/server.key'),
+    cert: fs.readFileSync('./certs/cert.crt')
+}, app)
+.listen(PORT, (err) => {   
     if(err) {
     debug(`error!!!!!!!:${err}`)
         throw new Error(err)
